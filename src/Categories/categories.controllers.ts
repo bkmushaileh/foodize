67396842026c9594583d5b39983e116a3904ccd3
abdullah <aboodk841@gmail.com>
2025-09-11 20:42:35 +0300

    add all models and create CRUD for catagory

---
+++ src/Categories/categories.controllers.ts
@@ -0,0 +1,40 @@
+import { serverError } from "../Middleware/serverError";
+import Catagory from "../Models/Catagory";
+import { NextFunction, Request, Response } from "express";
+
+const createCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, recipes } = req.body;
+    if (!name) {
+      return next({ message: "Name is required!", status: 401 });
+    }
+    const category = await Catagory.create(req.body);
+    console.log("🚀 ~ createCategory ~ req.body:", req.body);
+    return res.status(201).json(category);
+  } catch (error) {
+    console.log(error);
+    return next(serverError);
+  }
+};
+
+const getCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const categories = await Catagory.find();
+
+    if (!categories) {
+      return next({ message: "Catagory notfound", status: 404 });
+    }
+    return res.status(200).json(categories);
+  } catch (error) {
+    return next(serverError);
+  }
+};
+export { createCategory, getCategories };
